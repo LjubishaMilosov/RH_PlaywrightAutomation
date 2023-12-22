9e8ba5fb15102996e3ab0799bd793db8282f3a1b
Ljubisha Milosov <ljubisa.milosov@gmail.com>
2023-12-23 01:22:03 +0800

    signin test

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -2,13 +2,27 @@ const {test, expect} = require('@playwright/test');
 
 test.only('Browser Context Playwright test', async ({browser})=> 
 {
+    
     const context = await browser.newContext();
     const page = await context.newPage();
+
+    const userName = page.locator('input#username');
+    const signIn =  page.locator('#signInBtn');
+
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-    await console.log(await page.title());
-    await page.locator('input#username').fill('rahulshetty');
+        console.log(await page.title());
+    await userName.fill('rahulshetty');
     await page.locator('[id="password"]').fill('learning');
-    await page.locator('#signInBtn').click();
+    await signIn.click();
     await page.locator("[style*='block']").textContent();
-    console.log(await page.locator("[style*='block']").textContent());
+        console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+    await userName.fill("");
+    await userName.fill("rahulshettyacademy");
+    await signIn.click();
+
+    console.log(await page.locator(".card-body a").first().textContent());
+    console.log(await page.locator(".card-body a").nth(1).textContent());
+        
+
 });
