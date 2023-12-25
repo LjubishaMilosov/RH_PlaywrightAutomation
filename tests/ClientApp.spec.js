94f07518e84694e9d407db8bd3b35d1ad482eff7
Ljubisha Milosov <ljubisa.milosov@gmail.com>
2023-12-26 03:34:25 +0800

    wait mechanisms commit

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -5,6 +5,11 @@ test.only('Browser Context Playwright test', async ({page})=>
     
     await page.goto("https://rahulshettyacademy.com/client");
     await page.locator("#userEmail").fill("anshika@gmail.com");
-
+    await page.locator("#userPassword").fill("Iamking@000");
+    await page.locator("[value='Login']").click();
+    //await page.waitForLoadState('networkidle');
+    await page.locator("card-body b").first().waitFor();
+    const titles = await page.locator("card-body b").allTextContents();
+    console.log(titles);
     
 });
