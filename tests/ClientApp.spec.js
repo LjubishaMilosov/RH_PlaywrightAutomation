5a8854e7b6b34d5aa5dfac1b222820b6f87b7cf3
Ljubisha Milosov <ljubisa.milosov@gmail.com>
2023-12-29 04:07:48 +0800

    Addto cart

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -2,7 +2,7 @@ const {test, expect} = require('@playwright/test');
 
 test.only('Client App Login', async ({page})=> 
 {
-    const productName = "Zara Coat 3";
+    const productName = "ZARA COAT 3";
     const products = page.locator(".card-body");
     await page.goto("https://rahulshettyacademy.com/client");
     await page.locator("#userEmail").fill("ljubisa.milosov@gmail.com");
@@ -13,15 +13,19 @@ test.only('Client App Login', async ({page})=>
     const titles = await page.locator(".card-body b").allTextContents();
     console.log(titles);
 
-    const count = products.count();
-    for(let i = 0; i < count; i++)
+    const count = await products.count();
+    for(let i = 0; i < count; ++i)
     {
-           if( products.nth(i).locator("b").textContent() === productName)
+           if(await products.nth(i).locator("b").textContent() === productName)
            {
             await products.nth(i).locator("text= Add To Cart").click();
+            break;
            }
-           break;
     }
     
+    await page.locator("[routerlink*='cart']").click();
+    await page.locator("div li").first().waitFor();
+    const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
+    expect(bool).toBeTruthy();
     
 });
