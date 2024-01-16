dbb65e4f734a862dd40269b92993fb370962453e
Ljubisha Milosov <ljubisa.milosov@gmail.com>
2024-01-17 03:49:23 +0800

    OrderApi

--- tests/WebAPIPart1.spec.js
+++ tests/WebAPIPart1.spec.js
@@ -1,10 +1,11 @@
 const {test, expect, request} = require('@playwright/test');
 const loginPayload = {userEmail: "ljubisa.milosov@gmail.com", userPassword: "Muirotanas24"};
-
+const orderPayLoad = {orders: [{country: "India", productOrderedId: "6581ca399fd99c85e8ee7f45"}]}
 let token;
+let orderId;
 test.beforeAll( async() => 
 {
-
+    //Login Api
     const apiContext = await request.newContext();
     const loginResponse = await apiContext.
     post("https://www.rahulshettyacademy.com/api/ecom/auth/login",
@@ -16,6 +17,21 @@ test.beforeAll( async() =>
     
     token = loginResponseJson.token;
     console.log(token);
+
+    //Order Api
+    const orderResponse = await apiContext.post("https://www.rahulshettyacademy.com/api/ecom/order/create-order",
+    {
+        data : orderPayLoad,
+        headers:{
+                'Authorization' : token,
+                'Content-Type' : 'application/json'
+                }
+
+    })
+    const orderResponseJson = await orderResponse.json();
+    console.log(orderResponseJson);
+    orderId = orderResponseJson.orders[0];
+
 });
 
 test.beforeEach( () => 
@@ -42,47 +58,48 @@ test('Place Order', async ({page})=>
     // await page.locator("[value='Login']").click();
     //await page.waitForLoadState('networkidle');
 
-    await page.locator(".card-body b").first().waitFor();
-    const titles = await page.locator(".card-body b").allTextContents();
-    console.log(titles);
+    // await page.locator(".card-body b").first().waitFor();
+    // const titles = await page.locator(".card-body b").allTextContents();
+    // console.log(titles);
 
-    const count = await products.count();
-    for(let i = 0; i < count; ++i)
-    {
-           if(await products.nth(i).locator("b").textContent() === productName)
-           {
-            await products.nth(i).locator("text= Add To Cart").click();
-            break;
-           }
-    }
+    // const count = await products.count();
+    // for(let i = 0; i < count; ++i)
+    // {
+    //        if(await products.nth(i).locator("b").textContent() === productName)
+    //        {
+    //         await products.nth(i).locator("text= Add To Cart").click();
+    //         break;
+    //        }
+    // }
     
-    await page.locator("[routerlink*='cart']").click();
-    await page.locator("div li").first().waitFor();
-    const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
-    expect(bool).toBeTruthy();
-    await page.locator("text=Checkout").click();
-    await page.locator("[placeholder='Select Country']").pressSequentially("ind");
-    const dropdown = page.locator("section.ta-results");
-    await dropdown.waitFor();
-    const optionsCount =await dropdown.locator("button").count();
+    // await page.locator("[routerlink*='cart']").click();
+    // await page.locator("div li").first().waitFor();
+    // const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
+    // expect(bool).toBeTruthy();
+    // await page.locator("text=Checkout").click();
+    // await page.locator("[placeholder='Select Country']").pressSequentially("ind");
+    // const dropdown = page.locator("section.ta-results");
+    // await dropdown.waitFor();
+    // const optionsCount =await dropdown.locator("button").count();
     
-    for(let i = 0; i < optionsCount; ++i)
-    {
-        const text = await dropdown.locator("button").nth(i).textContent();
+    // for(let i = 0; i < optionsCount; ++i)
+    // {
+    //     const text = await dropdown.locator("button").nth(i).textContent();
         
-        if(text === " India")
-        {
-            await dropdown.locator("button").nth(i).click();
+    //     if(text === " India")
+    //     {
+    //         await dropdown.locator("button").nth(i).click();
         
-            break;
-        }
-    }
+    //         break;
+    //     }
+    // }
 
-    await expect(page.locator(".user__name label")).toHaveText(email);
-    await page.locator(".action__submit").click();
-    await expect( page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
-    const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
-    console.log("Order ID is " + orderId);
+    // await expect(page.locator(".user__name label")).toHaveText(email);
+    // await page.locator(".action__submit").click();
+    // await expect( page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+
+    // const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+    // console.log("Order ID is " + orderId);
 
     await page.locator("button[routerlink*='myorders']").click();
     await page.locator("tbody").waitFor();
@@ -101,6 +118,7 @@ test('Place Order', async ({page})=>
     const orderIdDetails = await page.locator("div .col-text").textContent();
     expect(orderId.includes(orderIdDetails)).toBeTruthy()
 
-
-
 });
+
+//Verify if order created is showing in history page
+//Precondition 
