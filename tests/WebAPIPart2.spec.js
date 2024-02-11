ec1d0b0b5badcdd9f3d4c006a336217ebd972b5d
Ljubisha Milosov <ljubisa.milosov@gmail.com>
2024-02-12 01:03:53 +0800

    webApiPart2

--- tests/WebAPIPart2.spec.js
+++ tests/WebAPIPart2.spec.js
@@ -1,15 +1,28 @@
+// Login UI  -> .json
+// test browser -> .json, cart-, order-, orderdetails, orderhistory
+
+
 const {test, expect} = require('@playwright/test');
+
+test.beforeAll( async ({browser}) =>
+{
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/client");
+    await page.locator("#userEmail").fill("ljubisa.milosov@gmail.com");
+    await page.locator("#userPassword").fill("Muirotanas24");
+    await page.locator("[value='Login']").click();
+    await page.waitForLoadState('networkidle');
+    await context.storageState({path:'state.json'});
+
+})
 
 test.only('Client App Login', async ({page})=> 
 {
     const email = "ljubisa.milosov@gmail.com";
     const productName = "ZARA COAT 3";
     const products = page.locator(".card-body");
-    await page.goto("https://rahulshettyacademy.com/client");
-    await page.locator("#userEmail").fill(email);
-    await page.locator("#userPassword").fill("Muirotanas24");
-    await page.locator("[value='Login']").click();
-    //await page.waitForLoadState('networkidle');
+    
     await page.locator(".card-body b").first().waitFor();
     const titles = await page.locator(".card-body b").allTextContents();
     console.log(titles);
