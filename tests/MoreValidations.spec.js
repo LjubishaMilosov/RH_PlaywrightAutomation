cd41ec71bfb233bafc446533c32e4f20b12cca66
Ljubisha Milosov <ljubisa.milosov@gmail.com>
2024-03-05 01:48:43 +0800

    taking screenshot

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -23,4 +23,16 @@ test("Popup validations", async({page}) =>
     await framesPage.locator("li a[href='lifetime-access']:visible").click();
     const textCheck = await framesPage.locator(".text h2").textContent();
     console.log(textCheck.split(" ")[1]);
-})
+});
+
+test.only("Sreenshot and visual comparison validations", async({page}) =>{
+
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+   
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator('#displayed-text').screenshot({path: 'partialScreenshot.png'});
+    await page.locator("#hide-textbox").click();
+    await page.screenshot({path: 'screenshot.png'});
+    await expect(page.locator("#displayed-text")).toBeHidden();
+
+});
